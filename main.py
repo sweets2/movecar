37dcedc06b688660ee62e4b86594f6bbf0bece79
sweets2 <eswiet1@gmail.com>
2023-07-16 18:12:15 -0400

    dotenv try

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 from flask import Flask
+from dotenv import load_dotenv
 
-
+load_dotenv()
 app = Flask(__name__)
-'test'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 
 @app.route("/")
 def hello():
-    return "Congratulations, it's a web app!"
+    return app.config['SECRET_KEY']
