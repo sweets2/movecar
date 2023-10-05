4d43e3292d3333b47ed8385af3bc984bc256c473
sweets2 <eswiet1@gmail.com>
2023-10-05 15:50:53 -0400

    turn off debugging

--- app/main.py
+++ app/main.py
@@ -160,5 +160,5 @@ def get_addresses():
 
 
 if __name__ == '__main__':
-    # app.run
-    app.run(host='127.0.0.1',port=5000,debug=True)
+    app.run
+    # app.run(host='127.0.0.1',port=5000,debug=True)
